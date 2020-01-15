84998dee98889a0a81e0a98e05b3c40eba5b8ea9
Ridwan Nugroho <ridwan.nugroho@bsi.co.id>
2020-01-15 14:31:06 +0700

    init commit

---
+++ Hash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
