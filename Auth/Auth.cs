58231d9125f51ce852c840902412f97ed8c307e9
Ridwan Nugroho <ridwan.nugroho@bsi.co.id>
2020-01-16 10:22:39 +0700

    Auth v1

--- Auth/Auth.cs
+++ Auth/Auth.cs
@@ -3,57 +3,102 @@ using System.Collections.Generic;
 
 namespace SysAuth
 {
-    class User
+    public class Auth
     {
+        private bool _isLogin;
+        private bool _isGuest;
         public string Username;
-        private string password;
+        private string _password;
+        private string _userId;
         public string Password
-        {set{password = value;}}
-        private steing lastLogin;
+        {set{_password = value;}}
+        private string _lastLogin;
 
-        public Dictionary user()
+        public Auth(string username, string pass, string id)
         {
+            Username = username;
+            _password = pass;
+            _userId = id;
 
+        }
+
+        public int login(string username, string pass)
+        {
+            if(validate(username, pass))
+            {
+                _isLogin = true;
+                _isGuest = false;
+                _lastLogin = DateTime.Now.ToString();
+                return 200;
+            }
+            
+            else
+                return 403;
+        }
+
+        public bool validate(string username, string pass)
+        {
+            if(Username == username && pass == _password)
+                return true;
+
+            else
+                return false;
+        }
+
+        public Dictionary<string, object> user()
+        {
+            Dictionary<string, object> ret = new Dictionary<string, object>();
+            ret.Add("username", Username);
+            ret.Add("password", _password);
+            ret.Add("lastLogin", _lastLogin);
+            ret.Add("user ID", _userId);
+            if(_isLogin)
+                ret.Add("user status", "Not Guest");
+            
+            else
+                ret.Add("user status", "Guest");
+
+            foreach (var d in ret)
+            {
+                Console.WriteLine("{0} : {1}", d.Key, d.Value);
+            }
+            
+            Console.WriteLine();
+            return ret;
         }
 
         public string id()
         {
-
+            return _userId;
         }
 
         public string lastLogin()
         {
-
-        }
-    }
-
-    class Auth : User
-    {
-        public int login(Dictionary user)
-        {
-
-        }
-
-        public bool validate(Dictionary user)
-        {
-
+            return _lastLogin;
         }
 
         public int logout()
         {
+            if(_isLogin)
+            {
+                _isLogin = false;
+                _isGuest = true;
+                return 200;
+            }
 
+            else
+                return 403;
         }
-
-        
 
         public bool check()
         {
-
+            return _isLogin;
         }
 
         public bool guest()
         {
-
+            return _isGuest;
         }
     }
+
 }
--- Auth/Program.cs
+++ Auth/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using SysAuth;
 
-namespace Auth
+namespace AuthProg
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Auth user1 = new Auth(username: "user1", pass: "pass1", id: "user001");
+            Auth user2 = new Auth(username: "user2", pass: "pass2", id: "user002");
+            Auth user3 = new Auth(username: "user3", pass: "pass3", id: "user003");
+
+            user1.user();
+            user2.user();
+            user3.user();
+            Console.WriteLine("======== user1 login ========");
+            user1.login("user1", "pass1");
+            user1.user();
+            Console.WriteLine("======== user2 login ========");
+            user2.login("user2", "pass2");
+            user2.user();
+            Console.WriteLine("======== user3 login ========");
+            user3.login("user3", "pass3");
+            user3.user();
+
+            Console.WriteLine("======== user1 logout ========");
+            user1.logout();
+            user1.user();
+
         }
     }
 }
